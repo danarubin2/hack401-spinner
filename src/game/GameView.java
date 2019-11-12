73af0f74c8a26bda35455f24efaab0b8b867d2e8
Janelle <jayazeng@users.noreply.github.com>
2019-11-11 20:26:28 -0500

    figured out how to make spinnerView bigger

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -18,30 +18,9 @@ public class GameView extends JPanel{
 	public void setUpView() {
 		setLayout(new FlowLayout());
 		SpinnerView spinnerView = game.getSpinnerView();
+		//spinnerView.setPreferredSize(this.getWidth(), this.getHeight());
 
 		this.add(spinnerView);
 	}
-	public void drawCenteredCircle(Graphics g, int x, int y, int r) {
-		  x = x-(r/2);
-		  y = y-(r/2);
-		  g.fillOval(x,y,r,r);
-	}
-	public void paintComponent(Graphics g){
-		g.setColor(Color.RED);
-		int centerX = this.getWidth()/2;
-		int centerY = this.getHeight()/2;
-		int radius = centerY;
-		if (centerX < centerY) {
-			radius = centerX;
-		}
-		drawCenteredCircle(g, radius/5, centerY, radius/5);
-		drawCenteredCircle(g, centerX-radius/2, centerY-radius/2, radius/5);
-		drawCenteredCircle(g, centerX, radius/5, radius/5);
-		drawCenteredCircle(g, centerX+radius/2, centerY-radius/2, radius/5);
-		drawCenteredCircle(g, centerX*2-radius/5, centerY, radius/5);
-		drawCenteredCircle(g, centerX+radius/2, centerY+radius/2, radius/5);
-		drawCenteredCircle(g, centerX, centerY*2-radius/5, radius/5);
-		drawCenteredCircle(g, centerX-radius/2, centerY+radius/2, radius/5);
-		
-	}
+	
 }
--- src/game/SpinnerView.java
+++ src/game/SpinnerView.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 
@@ -19,11 +21,36 @@ public class SpinnerView extends JPanel implements KeyListener{
 	
 	public void setUpSpinner() {
 		setLayout(new FlowLayout());
+		setPreferredSize(new Dimension(400,350));
 		this.addKeyListener(new StopListener());
 	}
 	
 	public Spinner getSpinner() {
 		return this.spinner;
+	}
+	
+	public void drawCenteredCircle(Graphics g, int x, int y, int r) {
+		  x = x-(r/2);
+		  y = y-(r/2);
+		  g.fillOval(x,y,r,r);
+	}
+	public void paintComponent(Graphics g){
+		g.setColor(Color.RED);
+		int centerX = this.getWidth()/2;
+		int centerY = this.getHeight()/2;
+		int radius = centerY;
+		if (centerX < centerY) {
+			radius = centerX;
+		}
+		drawCenteredCircle(g, 2*radius/5, centerY, radius/5);
+		drawCenteredCircle(g, centerX-radius/2, centerY-radius/2, radius/5);
+		drawCenteredCircle(g, centerX, radius/5, radius/5);
+		drawCenteredCircle(g, centerX+radius/2, centerY-radius/2, radius/5);
+		drawCenteredCircle(g, centerX*2-2*radius/5, centerY, radius/5);
+		drawCenteredCircle(g, centerX+radius/2, centerY+radius/2, radius/5);
+		drawCenteredCircle(g, centerX, centerY*2-radius/5, radius/5);
+		drawCenteredCircle(g, centerX-radius/2, centerY+radius/2, radius/5);
+		
 	}
 
 	@Override
